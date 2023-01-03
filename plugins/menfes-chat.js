54b37aced0a6591797f691eabb73d65cf32a5c7f
Wudysoft <116274228+wudysoft@users.noreply.github.com>
2023-01-03 13:16:43 +0800

    Add files via upload

--- plugins/menfes-chat.js
+++ plugins/menfes-chat.js
@@ -1,14 +1,18 @@
 let handler = async(m, { conn, text, usedPrefix, command, args }) => {
 	// Batas
-    let mention = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : args[0] ? (args[0].replace(/[@ .+-]/g, '') + '@s.whatsapp.net') : ''
-	let txt = (args.length > 1 ? args.slice(1).join(' ') : '') || ''
+    let [jid, name, pesan] = text.split('|');
+    jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+    let data = (await conn.onWhatsApp(jid))[0] || {};
+    if (!data.exists) throw 'Nomer tidak terdaftar di whatsapp.';
+    if (jid == m.sender) throw 'tidak bisa mengirim pesan Menfes ke diri sendiri.'
+    
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || ''
 	let tujuan = `👋 Saya *${conn.user.name}*, Pesan Untuk Kamu
 👥 Dari : *PENGIRIM RAHASIA*
 
 ${htki} 💌 Pesan ${htka}
-${htjava} ${txt}
+${htjava} ${pesan}
 `
 	let cap = `${htki} PESAN RAHASIA ${htka}
 Anda Ingin Mengirimkan Pesan ke pacar/sahabat/teman/doi/
@@ -19,57 +23,57 @@ Contoh Penggunaan: ${usedPrefix + command} ${nomorown} pesan untuknya
 Contoh: ${usedPrefix + command} ${nomorown} hai`
 let suks = `Mengirim Pesan *${mime ? mime : 'Teks'}*
 👥 Dari : @${m.sender.replace(/@.+/, '')}
-👥 Untuk : @${mention.replace(/@.+/, '')}
+👥 Untuk : @${jid.replace(/@.+/, '')}
 
 ${htki} 💌 Pesan ${htka}
-${htjava} ${txt ? txt : 'Pesan Kosong'}
+${htjava} ${pesan ? pesan : 'Pesan Kosong'}
 `
     // Batas
     command = command.toLowerCase()
-    this.menfes = this.menfes ? this.menfes : {}
+    conn.menfes = conn.menfes ? conn.menfes : {}
     switch (command) {
         case 'menfesleave': {
-            let room = Object.values(this.menfes).find(room => room.check(m.sender))
-            if (!room) return this.sendButton(m.chat, '*Kamu tidak sedang berada di menfes chat*', author, null, [['Mulai Menfes', '.menfesstart']], null)
+            let room = Object.values(conn.menfes).find(room => room.check(m.sender))
+            if (!room) return conn.sendButton(m.chat, '*Kamu tidak sedang berada di menfes chat*', author, null, [['Mulai Menfes', '.menfesstart']], null)
             m.reply('Sukses Hapus Menfes')
             let other = room.other(m.sender)
-            if (other) await this.sendButton(other, room.b + ' *Meninggalkan chat*', author, null, [['Mulai Menfes', '.menfesstart']], null)
-            delete this.menfes[room.id]
+            if (other) await conn.sendButton(other, room.b + ' *Meninggalkan chat*', author, null, [['Mulai Menfes', '.menfesstart']], null)
+            delete conn.menfes[room.id]
             if (command === 'menfesleave') break
         }
         case 'menfesstart': {
-            if (Object.values(this.menfes).find(room => room.check(m.sender))) return this.sendButton(m.chat, '*Kamu masih berada di dalam menfes chat, menunggu Balasan*', author, null, [['Hapus Menfes', '.menfesleave']], null)
-            let room = Object.values(this.menfes).find(room => room.state === 'WAITING' && !room.check(m.sender))
+            if (Object.values(conn.menfes).find(room => room.check(m.sender))) return conn.sendButton(m.chat, '*Kamu masih berada di dalam menfes chat, menunggu Balasan*', author, null, [['Hapus Menfes', '.menfesleave']], null)
+            let room = Object.values(conn.menfes).find(room => room.state === 'WAITING' && !room.check(m.sender))
             if (room) {
                 room.b = m.sender
                 room.state = 'CHATTING'
-                await this.sendButton(room.a, '*Menfes Chat Tersambung!*\nDengan: ' + m.sender, author, null, [['Hapus Menfes', '.menfesleave']], null)
-                await this.sendButton(m.sender, '*Menfes Chat Tersambung!*\nDengan: ' + room.a, author, null, [['Hapus Menfes', '.menfesleave']], null)
+                await conn.sendButton(room.a, '*Menfes Chat Tersambung!*\nDengan: ' + m.sender, author, null, [['Hapus Menfes', '.menfesleave']], null)
+                await conn.sendButton(m.sender, '*Menfes Chat Tersambung!*\nDengan: ' + room.a, author, null, [['Hapus Menfes', '.menfesleave']], null)
             } else {
             // Batas
 	if (!m.quoted) {
-		await conn.sendButton(mention, tujuan, cap, null, [['B A L A S', '.menfesstart']], null)
+		await conn.sendButton(jid, tujuan, cap, null, [['B A L A S', '.menfesstart']], null)
 	} else {
-		await conn.sendButton(mention, tujuan, cap, null, [['B A L A S', '.menfesstart']], null)
+		await conn.sendButton(jid, tujuan, cap, null, [['B A L A S', '.menfesstart']], null)
 		let media = q ? await m.getQuotedObj() : false || m
-		await conn.copyNForward(mention, media, false).catch(_ => _)
+		await conn.copyNForward(jid, media, false).catch(_ => _)
 	}
 	await conn.sendButton(m.chat, suks, wm, null, [['Tes Bot', 'tes']], m, { mentions: conn.parseMention(suks) })
             // Batas
                 let id = + new Date
-                this.menfes[id] = {
+                conn.menfes[id] = {
                     id,
                     a: m.sender,
                     b: '',
                     state: 'WAITING',
                     check: function (who = '') {
-                        return [this.a, this.b].includes(who)
+                        return [conn.a, conn.b].includes(who)
                     },
                     other: function (who = '') {
-                        return who === this.a ? this.b : who === this.b ? this.a : ''
+                        return who === conn.a ? conn.b : who === conn.b ? conn.a : ''
                     },
                 }
-                await this.sendButton(m.chat, '*Menunggu Balasan...*', author, null, [['Hapus Menfes', '.menfesleave']], null)
+                await conn.sendButton(m.chat, '*Menunggu Balasan...*', author, null, [['Hapus Menfes', '.menfesleave']], null)
             }
             break
         }
--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,37 +1,24 @@
 import fetch from 'node-fetch'
 import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
-try {
   if (!text) throw `Use example ${usedPrefix}${command} gustixa`
-  let vid = (await youtubeSearch(text)).video[0]
+  let ply = await youtubeSearch(text)
+  let vid = ply.video[0]
   if (!vid) throw 'Video/Audio Tidak ditemukan'
   let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
   const url = 'https://www.youtube.com/watch?v=' + videoId
-  await conn.sendHydrated(m.chat, `*YOUTUBE DOWNLOADER*
+  let des = `*YOUTUBE DOWNLOADER*
 📌 *Title:* ${title}
 🔗 *Url:* ${url}
 🖹 *Description:* ${description}
 ⏲️ *Published:* ${publishedTime}
 ⌚ *Duration:* ${durationH}
 👁️ *Views:* ${viewH}
-  `.trim(), author, thumbnail, url, '📺Go To Youtube!', null, null, [
-    ['Audio 🎧', `${usedPrefix}yta ${url} yes`],
-    ['Video 🎥', `${usedPrefix}ytv ${url} yes`],
-    ['Youtube Search🔎', `${usedPrefix}yts ${url}`]
-  ], m)
-} catch {
-if (!text) throw 'Input Query'
-  let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak Ditemukan'
-  let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
-  let url = 'https://www.youtube.com/watch?v=' + videoId
-  let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
-  let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Url:* ${url}`
-  let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
-  let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
-  // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
-  conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
-}
+  `.trim()
+  await conn.sendButton(m.chat, des, wm, thumbnail, [
+   ['Audio 🎧', `${usedPrefix}yta ${url} yes`],
+   ['Video 🎥', `${usedPrefix}ytv ${url} yes`]
+   ], m)
 
 }
 handler.help = ['play', 'play'].map(v => v + ' <pencarian>')
